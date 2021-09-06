deffa5840da5661141d1108f41ff1845d004b665
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-09-06 20:31:29 +0300

    lesson 17: simple task possible answers

---
+++ JavaScript/17 pamoka - spausdinimas ir kintamieji/task1.js
@@ -0,0 +1,24 @@
+// 1.
+console.log('Žilvinas VIdmantas');
+// 2.
+const age = 28;
+console.log(age);
+// 3. 
+const fatherAge = 48;
+const motherAge = 48;
+const sister1Age = 30;
+const sister2Age = 23;
+const sister3Age = 8;
+
+console.log({
+    fatherAge,
+    motherAge,
+    sister1Age,
+    sister2Age,
+    sister3Age
+});
+
+// 4.
+const avg = (age, fatherAge + motherAge + sister1Age + sister2Age + sister3Age) / 6;
+console.log({ avg });
+

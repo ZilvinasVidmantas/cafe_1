2887508f49940a388a3bedbf8358ec66b1140d74
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-09-06 20:10:49 +0300

    lesson 17: first JS task

--- JavaScript/17 pamoka - spausdinimas ir kintamieji/data-types.js
+++ JavaScript/17 pamoka - spausdinimas ir kintamieji/data-types.js
@@ -1,10 +1,10 @@
 /** 
  * Kintamasis tai tiesiog būdas pasiekti reikšę, bet pati reikšmė turi savo tipą.
- * JavaScript kalboje, visi tipai yra skirstomi į 2 grupes
+ * JavaScript kalboje, visi tipai yra skirstomi į 2 grupes"
  * Primityvūs:
  *    Number: bet koks skaičius
  *    String: simbolių darinys rašomas tarp kabučių <'...'>, arba <"...">, arba <`...`>
- *    Loginis: true || false. Bulio algrebra grėstos reikšmės - tiesa, arba melas.
+ *    Loginis: true || false. Bulio algrebra grįstos reikšmės - tiesa, arba melas.
  *    undefined: reikšmės neaprašymas, tuštuma.
  *    null: pasakymas, kad reikšmės nėra
  * Nuorodos tipo:
@@ -17,10 +17,10 @@
 
 console.group('Primityvių kintamųjų kopijavimas');
 {
-  const a = 1;
+  const a = 16;
   console.log('a:', a);
   let b = a;
-  console.log('Padaryta a kintamojo kopija b:', b);
+  console.log('Padaryta a kintamojo kopija į b:', b);
   b = 7;
   console.log('Pakeista b kintamojo reikšmė...');
   console.log('a:', a);
@@ -39,13 +39,14 @@ console.group('Nuorodos(objekto) tipo kintamųjų kopijavimas');
   console.log('a:', a);
   let b = a;
   console.log('Padaryta a kintamojo kopija b:', b);
-  b = 7;
+  b[1] = null;
   console.log('Pakeistos b masyvo reikšmės...');
   console.log('a:', a);
   console.log('b:', b);
-  console.log('Išvada: Pagal kopijavimo pavyzdį, matome, jog kintamieji nėra susiję.');
-  console.log('Pakeitus vieną - kitas nekinta.');
+  console.log('Išvada: Pagal kopijavimo pavyzdį, matome, jog kintamieji yra susiję.');
+  console.log('Pakeitus vieną - keičiasi abu.');
 }
 console.log('-------------------------');
 console.log();
 console.groupEnd();
+// 20:05

1962318f5408a773910af1f6c6687a064c9a978d
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-09-07 21:58:19 +0300

    lesson 18: cycle task done

--- JavaScript/18 pamoka - objektai, masyvai ir ciklai/exercises.js
+++ JavaScript/18 pamoka - objektai, masyvai ir ciklai/exercises.js
@@ -1,13 +1,13 @@
 const numbers = [0, 1, 2, 5, 6, 8, 9, 7, 5, 6];
 
-console.group('1. Parašykite ciklą, kuris atspausdintų, kiekvieną masyvo <numbers> elementą')
+console.group('1. Parašykite ciklą, kuris atspausdintų, kiekvieną masyvo <numbers> elementą');
 for (let i = 0; i < numbers.length; i++) {
   console.log(numbers[i]);
 }
 console.groupEnd();
 console.log();
 
-console.group('2. Parašykite ciklą, kuris atspausdintų, kiekvieną masyvo <numbers> elementą iš kito galo')
+console.group('2. Parašykite ciklą, kuris atspausdintų, kiekvieną masyvo <numbers> elementą iš kito galo');
 for (let i = numbers.length - 1; i >= 0; i--) {
   console.log(numbers[i]);
 }
@@ -15,21 +15,29 @@ console.groupEnd();
 console.log();
 
 console.group('3. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant pirmu');
-// code ...
+for (let i = 0; i < numbers.length; i += 2) {
+  console.log(numbers[i]);
+}
 console.groupEnd();
 console.log();
 
 console.group('4. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant antru');
-// code ...
+for (let i = 1; i < numbers.length; i += 2) {
+  console.log(numbers[i]);
+}
 console.groupEnd();
 console.log();
 
 console.group('5. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant pirmu iš kito galo');
-// code ...
+for (let i = numbers.length - 1; i >= 0; i-=2) {
+  console.log(numbers[i]);
+}
 console.groupEnd();
 console.log();
 
 console.group('6. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant antru iš kito galo');
-// code ...
+for (let i = numbers.length - 2; i >= 0; i-=2) {
+  console.log(numbers[i]);
+}
 console.groupEnd();
 console.log();

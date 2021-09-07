ae6060f1fc8a294ac6c544d8c8f4b081bfe5316f
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-09-07 21:36:12 +0300

    lesson 18: for cycles

---
+++ JavaScript/18 pamoka - objektai, masyvai ir ciklai/exercises.js
@@ -0,0 +1,7 @@
+const numbers = [0, 1, 2, 5, 6, 8, 9, 7, 5, 6];
+// 1. Parašykite ciklą, kuris atspausdintų, kiekvieną masyvo <numbers> elementą
+// 2. Parašykite ciklą, kuris atspausdintų, kiekvieną masyvo <numbers> elementą iš kito galo
+// 3. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant pirmu
+// 4. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant antru
+// 5. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant pirmu iš kito galo
+// 6. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant antru iš kito galo

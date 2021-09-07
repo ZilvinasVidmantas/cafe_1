e3e42f643ab32a97c9cce8e7262dddc536667d8e
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-09-07 21:46:05 +0300

    lesson 18: cycle task hints

--- JavaScript/18 pamoka - objektai, masyvai ir ciklai/exercises.js
+++ JavaScript/18 pamoka - objektai, masyvai ir ciklai/exercises.js
@@ -1,7 +1,35 @@
 const numbers = [0, 1, 2, 5, 6, 8, 9, 7, 5, 6];
-// 1. Parašykite ciklą, kuris atspausdintų, kiekvieną masyvo <numbers> elementą
-// 2. Parašykite ciklą, kuris atspausdintų, kiekvieną masyvo <numbers> elementą iš kito galo
-// 3. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant pirmu
-// 4. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant antru
-// 5. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant pirmu iš kito galo
-// 6. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant antru iš kito galo
+
+console.group('1. Parašykite ciklą, kuris atspausdintų, kiekvieną masyvo <numbers> elementą')
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+console.groupEnd();
+console.log();
+
+console.group('2. Parašykite ciklą, kuris atspausdintų, kiekvieną masyvo <numbers> elementą iš kito galo')
+for (let i = numbers.length - 1; i >= 0; i--) {
+  console.log(numbers[i]);
+}
+console.groupEnd();
+console.log();
+
+console.group('3. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant pirmu');
+// code ...
+console.groupEnd();
+console.log();
+
+console.group('4. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant antru');
+// code ...
+console.groupEnd();
+console.log();
+
+console.group('5. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant pirmu iš kito galo');
+// code ...
+console.groupEnd();
+console.log();
+
+console.group('6. Parašykite ciklą, kuris atspausdintų, kas antrą masyvo <numbers> elementą pradedant antru iš kito galo');
+// code ...
+console.groupEnd();
+console.log();

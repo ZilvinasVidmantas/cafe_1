2aadb65b78529b6a75cafb8ec40097a07730bd08
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-09-14 19:02:29 +0300

    lesson 22: form submition in JS

---
+++ JavaScript/22 pamoka - formos/js/main.js
@@ -0,0 +1,7 @@
+function handleRegisterForm(event){
+  event.preventDefault();
+  // 19:13
+}
+
+const formRegister = document.querySelector('#formRegister');
+formRegister.addEventListener('submit', handleRegisterForm);

355b3da45304a94ca354254e86a60b124b1948a9
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-09-14 21:21:23 +0300

    refactor: deleted duplicate files

--- JavaScript/Atskiros temos kas nori anksciau/js/dinaminesSavybes.js
+++
@@ -1,9 +0,0 @@
-// Objekto savybių naudojimas, kuomet jas gauname string pavidalu
-const savybeKuriaNorimeKeisti = 'dydis';
-const kebabas = {
-  dydis: 'xl',
-  padazas: 'cesnakinis',
-  suStixais: true
-};
-console.log(kebabas.dydis, kebabas.padazas);
-console.log(kebabas[savybeKuriaNorimeKeisti]);

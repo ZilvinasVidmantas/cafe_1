abe796c2fa7501af81b47e0badaaa6ad9c60a24b
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-09-16 21:22:53 +0300

    lesson 24: classes

--- JavaScript/Atskiros temos/classes.js
+++ JavaScript/Atskiros temos/classes.js
@@ -1,10 +1,13 @@
 // Prototipo kūrimas naudojant 'class' raktažodį - ES5 'sintaksinis cukrus',
 // jog prototipų kūrimas būtų panašesnis į programuotojams įprastą sintaksę
+
 // Kintamieji, kurie yra priskiriami formuojamui objektui yra vadinami - savybėmis (properties)
-// Vidinės funkcijos, kurios priskiriamos objektui, ar jo prototipui yra vadinamos - metodais (methods)
-// Objektai sukurti pagal klasės(prototipo) šabloną programavimo literatūroje yra vadinami 
+// Vidinės funkcijos, kurios priskiriamos objektui, ar jo klasei yra vadinamos - metodais (methods)
+
+// Objektai sukurti pagal klasės šabloną programavimo literatūroje yra vadinami 
 //  * 'instance of a class', arba tiesiog 'instance'
-//  * object (kitose programavimo kalbose, obejktais yra vadinama - išskirta(sukurta) atminties vieta pagal klasės aprašą)
+//  * object (kitose programavimo kalbose, obejktais yra vadinama - išskirta(sukurta) atminties vieta pagal klasės šabloną)
+
 // lietuviškai artimiausiai apibūdinantys 'instance of a class' žodžiai galėtų būti:
 //  * pavyzdinis vientas
 //  * klasės šablono realizacija
@@ -12,16 +15,22 @@
 //  * objektas (naudojamas kitose programavimo kalbose) - tai ką grąžina klasės konstruktorius
 {
   class Car {
-    // Metodas skirtas objektui(pagal Car prototipą) kurti
+    // Metodas skirtas objektui kurti
+    // jis iškviečiamas kaskasrt, kai kuriame naują klasės objektą
     constructor(brand, model, year, color) {
       // Savybės ...
       this.brand = brand;
       this.model = model;
       this.year = year;
       this.color = color;
+      this.speed = 0;
     }
 
     // Metodai ....
+    accelerate(amount){
+      this.speed += amount;
+    }
+
     changeColor(color) {
       this.color = color;
     }
@@ -33,16 +42,17 @@
   const myCar = new Car('Peugeot', '406 Coupe', 1998, 'silver');
   const friendCar = new Car('Peogeot', '407', 2006, 'black');
   const uncleCar = new Car('BMW', '320d', 2006, 'black');
+  const allCars = [myCar, friendCar, uncleCar];
+  console.table(allCars);
 
+  allCars.forEach(car => car.accelerate(20));
   myCar.displayColor();
   myCar.changeColor('red');
   friendCar.changeColor('blue');
   friendCar.changeColor('green');
   myCar.displayColor();
 
-  const allCars = [myCar, friendCar, uncleCar];
   console.table(allCars);
-  console.log(myCar);
 }
 
 // Klasės savybių ir metodų rašymo tvarka
@@ -61,3 +71,5 @@ class AXY {
     savybes naudojantys metodai (dažniausiai spausdinimas)
 }
 */
+
+// 21:32

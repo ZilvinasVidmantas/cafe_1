efab33c927e913e0ab8048d6d2d121e5a15089e1
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-04 18:03:39 +0300

    lesson 33: init

---
+++ JavaScript/33 pamoka - localStorage autorizacija 4/js/services/LocalStorageService.js
@@ -0,0 +1,79 @@
+class LocalStorageService {
+  constructor(root) {
+    this.root = root;
+    const data = localStorage[root];
+    // Senesnė alternatyvos 4 eilutei 
+    { // ↓↓↓↓↓↓↓↓↓↓↓ IŠTRINK MANE ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+      const data = window.localStorage.getItem(root);
+    } // ↑↑↑↑↑↑↑↑↑↑↑ IŠTRINK MANE ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+    if (data) {
+      this.data = JSON.parse(data);
+    } else {
+      this.data = {};
+      this.synchronize();
+    }
+  }
+
+  generateId = (collectionName) => {
+    const idArray = Object.keys(this.data[collectionName]);
+    if (idArray.length === 0) return '0';
+    const max = Math.max(...idArray.map(id => Number(id)));
+    return String(max + 1);
+  }
+
+  synchronize = () => {
+    localStorage[this.root] = JSON.stringify(this.data);
+    // Senesnė alternatyvos 25 eilutei 
+    { // ↓↓↓↓↓↓↓↓↓↓↓ IŠTRINK MANE ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+      window.localStorage.setItem(this.root, JSON.stringify(this.data));
+    } // ↑↑↑↑↑↑↑↑↑↑↑ IŠTRINK MANE ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+  }
+
+  addCollection = (collectionName) => {
+    if (!this.data[collectionName]) {
+      this.data[collectionName] = {};
+      this.synchronize();
+    }
+  }
+
+  removeCollection = (collectionName) => {
+    if (this.data[collectionName]) {
+      delete this.data[collectionName];
+      this.synchronize();
+    }
+  }
+
+  addItem = (item, collectionName) => {
+    if (!this.data[collectionName]) {
+      this.addCollection(collectionName);
+    }
+    const newId = this.generateId(collectionName);
+    this.data[collectionName][newId] = item;
+    this.synchronize();
+    return newId;
+  }
+
+  removeItem = (id, collectionName) => {
+    if (this.data[collectionName]) {
+      delete this.data[collectionName][id];
+      this.synchronize();
+    }
+  }
+
+  getCollection = (collectionName) => {
+    if (this.data[collectionName]) {
+      return Object.entries(this.data[collectionName]).map(([id, data]) => ({ id, data }));
+    }
+    return null;
+  }
+
+  getItem = (id, collectionName) => {
+    if (this.data[collectionName] && this.data[collectionName][id]) {
+      return {
+        id: String(id),
+        data: this.data[collectionName][id]
+      }
+    }
+    return null;
+  }
+}

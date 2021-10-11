8778e43011ea31d74f149fe602473eb625f2ce5a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-11 21:54:29 +0300

    lesson 35: HTML basics

--- JavaScript/35 pamoka - kontrolinis ir aptarimas/dom-functions.js
+++ JavaScript/35 pamoka - kontrolinis ir aptarimas/dom-functions.js
@@ -1,25 +1,23 @@
 const btnInc = document.querySelector('.btn-primary');
 const btnDec = document.querySelector('.btn-warning');
+const colorPicker = document.querySelector('.form-control-color');
 const square = document.querySelector('.square');
 
 const sideDelta = 10;
+let squareSize = square.getBoundingClientRect().height;
+square.style.backgroundColor = colorPicker.value;
 
-const incSquareSize = () => {
-  const coundingRect = square.getBoundingClientRect();
-  const currentSize = coundingRect.height;
-  square.style.height = currentSize + sideDelta + 'px';
-  square.style.width = currentSize + sideDelta + 'px';
+const changeSquareSize = (direction = 1) => {
+  squareSize += sideDelta * direction;
+  const sideSizeInPixels = squareSize + 'px';
+  square.style.height = sideSizeInPixels;
+  square.style.width = sideSizeInPixels;
 }
 
-const decSquareSize = () => {
-  console.log('Kvadratas mažinamas');
-}
-
-btnInc.addEventListener('click', incSquareSize);
-btnDec.addEventListener('click', decSquareSize);
+btnInc.addEventListener('click', () => changeSquareSize());
+btnDec.addEventListener('click', () => changeSquareSize(-1));
+colorPicker.addEventListener('change', () => square.style.backgroundColor = colorPicker.value);
 
 /*
-  Pertrauka iki 21:20
-  21:20 - 21:25 įgalinti mažinimo logiką
-
+  Sukurti dar vieną spalvos parinkimo įvestį, kuri nustatytų kvadrato sienelės spalvą
 */

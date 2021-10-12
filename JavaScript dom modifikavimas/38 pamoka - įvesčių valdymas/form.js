b874b55da00cf587e80ea9cd4f8adf725607b208
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-12 18:49:07 +0300

    lesson 38: formm field data task

--- JavaScript dom modifikavimas/38 pamoka - įvesčių valdymas/form.js
+++ JavaScript dom modifikavimas/38 pamoka - įvesčių valdymas/form.js
@@ -1,2 +1,19 @@
 const registerForm = document.querySelector('#register-form');
 const registerFormResultContainer = document.querySelector('#register-form-result-container');
+
+// 1. Vykdyti funkciją, kuomet paspaudžiamas "submit mygtukas". Submit'inama forma <registerForm>
+const handleRegisterForm = (event) => {
+  event.preventDefault();
+  // 2. Nuskaityti formos įvesčių duomenis
+  /*
+    2.1 - Atspausdinti visus formoje esančius įvesties laukus
+    2.2 - Atspausdinti visų formoje esančių įvestie laukų reikšmes
+ 
+  */
+}
+
+registerForm.addEventListener('submit', handleRegisterForm);
+
+
+// 3. Suformuoti formos duomenis reikiamu formatu
+// 4. Pateikti duomenis į rezultato elementą <registerFormResultContainer>

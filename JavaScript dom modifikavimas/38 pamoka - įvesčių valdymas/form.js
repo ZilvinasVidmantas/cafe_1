6954205764cc53c39fb343c266fb302384efa56c
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-12 20:53:13 +0300

    lesson 38: form fields formatted

--- JavaScript dom modifikavimas/38 pamoka - įvesčių valdymas/form.js
+++ JavaScript dom modifikavimas/38 pamoka - įvesčių valdymas/form.js
@@ -1,28 +1,25 @@
 const registerForm = document.querySelector('#register-form');
 const registerFormResultContainer = document.querySelector('#register-form-result-container');
-const registerFormInputName = registerForm.querySelector('#register-name');
-const registerFormInputLastname = registerForm.querySelector('#register-lastname');
-const registerFormInputEmail = registerForm.querySelector('#register-email');
-const registerFormInputCity = registerForm.querySelector('#register-city');
-const registerFormSelectState = registerForm.querySelector('#register-state');
-const registerFormInputZip = registerForm.querySelector('#register-zip');
 
-// 1. Vykdyti funkciją, kuomet paspaudžiamas "submit mygtukas". Submit'inama forma <registerForm>
 const handleRegisterForm = (event) => {
   event.preventDefault();
-  // 3. Suformuoti formos duomenis reikiamu formatu
-  const data = {
-    firstname: registerFormInputName.value,
-    lastname: registerFormInputLastname.value,
-    email: registerFormInputEmail.value,
-    city: registerFormInputCity.value,
-    state: registerFormSelectState.value,
-    zip: registerFormInputZip.value,
-  };
-  console.log(data);
+  const fieldValues = Array
+    .from(registerForm)
+    .filter(field => field.name)
+    .reduce((fieldValuesObject, field) => {
+      fieldValuesObject[field.name] = field.value;
+      return fieldValuesObject;
+    }, {});
+  renderResult(fieldValues);
+}
+
+const renderResult = fieldValues => {
+  let result = '<div>{</div>';
+  for (const fieldName in fieldValues) {
+    result += `<div class="ms-3"><strong>${fieldName}</strong><span>: ${fieldValues[fieldName]}</span></div>`;
+  }
+  result += '<div>}</div>';
+  registerFormResultContainer.innerHTML = result;
 }
 
 registerForm.addEventListener('submit', handleRegisterForm);
-
-
-// 4. Pateikti duomenis į rezultato elementą <registerFormResultContainer>

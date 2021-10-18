03bc183819fbbc527da6173d2a0f82bd3b39c2ac
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-18 19:00:13 +0300

    lesson 41: animal cards task

---
+++ JavaScript dom modifikavimas/41 pamoka - turinio kūrimas su JavaScript/js/carData.js
@@ -0,0 +1,58 @@
+const carData = [
+  {
+    brand: 'Tesla',
+    model: 'Model 3',
+    desc: 'Model 3 comes with the option of dual motor all-wheel drive, 20” Überturbine Wheels and Performance Brakes and lowered suspension for total control.',
+    imgSrc: 'https://s1.15min.lt/static/cache/NTU3eDI5MiwyNTR4MTcwLDYwMDQzNyxvcmlnaW5hbCwsaWQ9NTI0NTc4MCZkYXRlPTIwMjAlMkYwMiUyRjIxLDI1MjM1NzkwNTQ=/tesla-model-3-5e4f6b351eec5.jpg',
+    linkAddress: 'https://www.tesla.com/model3'
+  },
+  {
+    brand: 'Tesla',
+    model: 'Model S',
+    desc: 'With the longest range and quickest acceleration of any electric vehicle in production, Model S Plaid is the highest performing sedan ever built.',
+    imgSrc: 'https://s1.15min.lt/images/photos/2020/06/01/original/1-5ed4ee0156831.jpg',
+    linkAddress: 'https://www.tesla.com/models'
+  },
+  {
+    brand: 'Tesla',
+    model: 'Model 3',
+    desc: 'Model 3 comes with the option of dual motor all-wheel drive, 20” Überturbine Wheels and Performance Brakes and lowered suspension for total control.',
+    imgSrc: 'https://s1.15min.lt/static/cache/NTU3eDI5MiwyNTR4MTcwLDYwMDQzNyxvcmlnaW5hbCwsaWQ9NTI0NTc4MCZkYXRlPTIwMjAlMkYwMiUyRjIxLDI1MjM1NzkwNTQ=/tesla-model-3-5e4f6b351eec5.jpg',
+    linkAddress: 'https://www.tesla.com/model3'
+  },
+  {
+    brand: 'Tesla',
+    model: 'Model S',
+    desc: 'With the longest range and quickest acceleration of any electric vehicle in production, Model S Plaid is the highest performing sedan ever built.',
+    imgSrc: 'https://s1.15min.lt/images/photos/2020/06/01/original/1-5ed4ee0156831.jpg',
+    linkAddress: 'https://www.tesla.com/models'
+  },
+  {
+    brand: 'Tesla',
+    model: 'Model 3',
+    desc: 'Model 3 comes with the option of dual motor all-wheel drive, 20” Überturbine Wheels and Performance Brakes and lowered suspension for total control.',
+    imgSrc: 'https://s1.15min.lt/static/cache/NTU3eDI5MiwyNTR4MTcwLDYwMDQzNyxvcmlnaW5hbCwsaWQ9NTI0NTc4MCZkYXRlPTIwMjAlMkYwMiUyRjIxLDI1MjM1NzkwNTQ=/tesla-model-3-5e4f6b351eec5.jpg',
+    linkAddress: 'https://www.tesla.com/model3'
+  },
+  {
+    brand: 'Tesla',
+    model: 'Model S',
+    desc: 'With the longest range and quickest acceleration of any electric vehicle in production, Model S Plaid is the highest performing sedan ever built.',
+    imgSrc: 'https://s1.15min.lt/images/photos/2020/06/01/original/1-5ed4ee0156831.jpg',
+    linkAddress: 'https://www.tesla.com/models'
+  },
+  {
+    brand: 'Tesla',
+    model: 'Model 3',
+    desc: 'Model 3 comes with the option of dual motor all-wheel drive, 20” Überturbine Wheels and Performance Brakes and lowered suspension for total control.',
+    imgSrc: 'https://s1.15min.lt/static/cache/NTU3eDI5MiwyNTR4MTcwLDYwMDQzNyxvcmlnaW5hbCwsaWQ9NTI0NTc4MCZkYXRlPTIwMjAlMkYwMiUyRjIxLDI1MjM1NzkwNTQ=/tesla-model-3-5e4f6b351eec5.jpg',
+    linkAddress: 'https://www.tesla.com/model3'
+  },
+  {
+    brand: 'Tesla',
+    model: 'Model S',
+    desc: 'With the longest range and quickest acceleration of any electric vehicle in production, Model S Plaid is the highest performing sedan ever built.',
+    imgSrc: 'https://s1.15min.lt/images/photos/2020/06/01/original/1-5ed4ee0156831.jpg',
+    linkAddress: 'https://www.tesla.com/models'
+  },
+];
---
+++ JavaScript dom modifikavimas/41 pamoka - turinio kūrimas su JavaScript/js/main.js
@@ -0,0 +1,39 @@
+const cardContainer = document.querySelector('#card-container');
+
+// 1. pagal duomenis sukurti koreteles
+const carCards = carData.map(({ brand, model, desc, imgSrc, linkAddress }) => {
+  return new Card({
+    desc,
+    imgSrc,
+    title: brand + ' ' + model,
+    href: linkAddress,
+  })
+});
+
+// 2. įdėti korteles į stulpelius
+const columns = carCards.map(card => {
+  const col = document.createElement('div');
+  col.className = 'col-12 col-md-6 col-lg-4 col-xl-3';
+  col.appendChild(card.element);
+  return col;
+})
+
+// 3. stulpelius įdėti į <cardContainer>
+columns.forEach(col => cardContainer.appendChild(col));
+
+// ---------------------------------------------------------------
+/*
+  1. Sukurti Gyvūnų duomenis  faile animalData.js
+    8 įrašai su savybėmis:
+      name: String,
+      kind: String,
+      about: String,
+      imgSrc: String,
+      link: String
+  
+  2. Sukurti Korteles pagal gyvūnų duomenis
+
+  3. įdėti koreteles į stulpelius
+
+  4. Stulpelius įdėti į cardContainer
+*/

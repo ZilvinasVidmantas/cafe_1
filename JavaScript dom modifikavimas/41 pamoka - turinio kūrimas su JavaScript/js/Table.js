d541e330a7a1ea5a473a8d14a1e4695e5265b235
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-18 21:02:47 +0300

    lesson 41: hints

--- JavaScript dom modifikavimas/41 pamoka - turinio kūrimas su JavaScript/js/Table.js
+++ JavaScript dom modifikavimas/41 pamoka - turinio kūrimas su JavaScript/js/Table.js
@@ -1,13 +1,12 @@
 class Table {
   static formColumnStyle = (columnCount) => {
+    return [...Array(columnCount)].map(_ => 'auto').join(' ');
     let gridTemplateColumns = '';
     for (let i = 0; i < columnCount; i++) {
       gridTemplateColumns += 'auto ';
     }
     return gridTemplateColumns.slice(0, gridTemplateColumns.length);
-    return [...Array(columnCount).keys()].map(_ => 'auto').join(' ');
   }
-
 
   constructor({ columns, rows }) {
     /*
@@ -26,20 +25,30 @@ class Table {
     this.element = document.createElement('div');
     this.element.style.display = 'grid';
     this.element.style.gridTemplateColumns = Table.formColumnStyle(columns.length);
-    this.createHeader();
+    this.render();
   }
 
-  createHeader = () => {
-    /*
-      1. iteruoti per savybę this.columns
-      2. su kiekvienu stulpelio pavadinimu, sukurti div
-      3. įdėti stulpeliu pavadinimą į sukurtą div [2.]
-      4. uždėti juodą foną
-      5. uždėti baltas raides
-      6. uždėti paddingo 1rem
-      7. prijungti prie this.element
-    */
-
+  renderHeader = () => {
+    // 1. iteruoti per savybę this.columns
+    this.????(columnName => {
+      // 2. su kiekvienu stulpelio pavadinimu, sukurti div
+      const col = document.?????('div');
+      // 3. įdėti stulpelio pavadinimą į sukurtą div [2.]
+      col.????? = columnName;
+      // 4. uždėti juodą foną
+      col.style.backgro??? = '#000';
+      // 5. uždėti baltas raides
+      col.style.???? = '#000';
+      // 6. uždėti paddingo 1rem
+      col.style.???? = '1rem';
+      // 7. prijungti prie this.element
+      this.element.?????(col);
+    });
     console.log(this.element.outerHTML)
+  }
+
+  render = () => {
+    this.renderHeader();
+    this.renderData();
   }
 }

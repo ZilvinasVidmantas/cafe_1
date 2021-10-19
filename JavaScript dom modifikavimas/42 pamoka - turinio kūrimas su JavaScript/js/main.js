42cd7d3bf162bd0ff73a218d276250fc50afe9ae
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-19 19:37:07 +0300

    lesson 42: data formatted for Table Component

--- JavaScript dom modifikavimas/42 pamoka - turinio kūrimas su JavaScript/js/main.js
+++ JavaScript dom modifikavimas/42 pamoka - turinio kūrimas su JavaScript/js/main.js
@@ -1,5 +1,6 @@
 const rootContainer = document.querySelector('#root');
 
+// --------------------------------------- Kortelių kūrimas -----------------------------------------------
 const carCards = carData.map(({ brand, model, desc, imgSrc, linkAddress }) => new Card({
   desc,
   imgSrc,
@@ -16,8 +17,9 @@ const animalCards = animalData.map(({ name, kind, about, imgSrc, link }) => new 
 }));
 const animalCardContainer = new CardContainer(animalCards);
 
+// ---------------------------------------- Lentelių kūrimas ------------------------------------------------
 const wordLimit = 55;
-const table1 = new Table( {
+const carTable = new Table( {
   columns: ['Image', 'Brand', 'Model', 'Description', 'Link'],
   rows: carData.reduce((result, { brand, model, desc, imgSrc, linkAddress }) => {
     const descWords = desc.split(' ');
@@ -33,30 +35,24 @@ const table1 = new Table( {
   }, [])
 });
 
-const table2 = new Table({
-  columns: ['Vardas', 'Pavardė', 'Svoris (kg)', 'Ūgis (m)'],
-  rows: [
-    ['Kevinas', 'Barbenis', '80', '1.80'],
-    ['Kepalas', 'Barbenis', '80', '1.80'],
-    ['Ganatolijus', 'Barbenis', '80', '1.80'],
-    ['Kreimeris', 'Barbenis', '80', '1.80'],
-    ['Supačius', 'Barbenis', '80', '1.80'],
-    ['Faurina', 'Barbenis', '80', '1.80'],
-    ['Motelė', 'Barbenis', '80', '1.80'],
-    ['Zaubra', 'Barbenis', '80', '1.80'],
-    ['Čigonas', 'Barbenis', '80', '1.80'],
-  ]
+const animalTable = new Table({
+  columns: ['Image', 'Name', 'Kind', 'About', 'Link'],
+  rows: animalData.reduce((result, { name, kind, about, imgSrc, link}) => {
+    const rowData = [
+      `<img src="${imgSrc}" style="height: 80px; width: 80px; object-fit: cover;"/>`,
+      name,
+      kind,
+      about,
+      `<a class="btn btn-primary" href="${link}" target="blank">More info</a>`
+    ];
+    result.push(rowData); 
+    return result;
+  }, [])
 });
 
+// -------------------------------- Komponentų įdėjimas į DOM'e esantį elementą -------------------------------------
 // rootContainer.appendChild(carCardContainer.element);
 // rootContainer.appendChild(animalCardContainer.element);
-rootContainer.appendChild(table1.element);
-// rootContainer.appendChild(table2.element);
+// rootContainer.appendChild(carTable.element);
+rootContainer.appendChild(animalTable.element);
 
-
-/*
-  10 min pertrauka
-  Pažymime MS Teams formoj, kad dar NEPADARĖME užduoties
-  Suformuojame gyvūnų lentelę pagal <animalData> duomenis
-  Pažymime MS Teams formoj, kad dar PADARĖME užduoties
-*/

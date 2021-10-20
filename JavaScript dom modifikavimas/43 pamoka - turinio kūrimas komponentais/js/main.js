a437027370854f0596c841d1382e2a4798881cf4
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-20 19:15:24 +0300

    lesson 43: PersonCard coponent HTML

---
+++ JavaScript dom modifikavimas/43 pamoka - turinio kūrimas komponentais/js/main.js
@@ -0,0 +1,20 @@
+// 1. Sukurkite kortelės komponentą <PersonCard> vienam žmogui: 'task-1.png';
+const personData = {
+  name: 'Mantas',
+  surname: 'Kačerauskas',
+  title: 'Direktorius',
+  email: 'mantas@prodivi.lt',
+  mobile: '+370 672 89865',
+  linkedIn: 'https://www.linkedin.com/in/mantas-ka%C4%8Derauskas-295648aa/'
+}
+const rootContainer = document.querySelector('#root');
+/* Pasireikškite*/
+
+
+// 2. Sukurkite 3 koretelės komponentus, viens šalia kito per vidurį ekrano, su 50px tarpais.
+//   Šių komponentų išdėstymui sukurkite komponentą <PeopleContainer>.
+
+const peopleData = [
+  /* Pasireikškite*/
+];
+/* Pasireikškite*/

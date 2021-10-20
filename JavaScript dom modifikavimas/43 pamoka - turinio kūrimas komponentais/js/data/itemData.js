9734775a1f22283b423b7d463414391fa4cc433c
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-20 21:56:45 +0300

    lesson 43: itemData

---
+++ JavaScript dom modifikavimas/43 pamoka - turinio kūrimas komponentais/js/data/itemData.js
@@ -0,0 +1,56 @@
+const itemData = [
+  {
+    bestSeller: 'In Christmas',
+    link: 'https://www.amazon.com/Assorted-Watercolor-Stationary-Notecards-Envelopes/dp/B08HLXGS6N/ref=sr_1_29?dchild=1&keywords=cards&qid=1634753011&sr=8-29',
+    imgSrc: 'https://m.media-amazon.com/images/I/816m0Iw-E4L._AC_UL320_.jpg',
+    desc: 'Silver and high quality moissanite, which is made to last a lifetime. The earrings post are firmly soldered, which make them strong and durable in daily wearing. These earrings are also skin friendly and they will not hurt your skin',
+    rating: {
+      1: 54,
+      2: 74,
+      3: 206,
+      4: 102,
+      5: 1144
+    },
+    price: {
+      currency: {
+        name: 'USD',
+        symbol: '$',
+      },
+      value: 10.99
+    },
+    discounts: [
+      { type: 'specific', value: 7.99 },
+      { type: 'coupon', percent: 5 }
+    ],
+    shipsToLithuania: true,
+    stock: {
+      new: 7,
+      used: [9.99, 7.99, 8.49]
+    },
+    ageRestriction: '3 years and up',
+  },
+  {
+    imgSrc: 'https://unsplash.it/400/401',
+    link: 'https://www.amazon.com/Assorted-Watercolor-Stationary-Notecards-Envelopes/dp/B08HLXGS6N/ref=sr_1_29?dchild=1&keywords=cards&qid=1634753011&sr=8-29',
+    desc: 'Silver and high quality moissanite',
+    rating: {
+      1: 54,
+      2: 74,
+      3: 206,
+      4: 102,
+      5: 1144
+    },
+    price: {
+      currency: {
+        name: 'USD',
+        symbol: '$',
+      },
+      min: 25,
+      max: 200
+    },
+    discounts: [
+      { type: 'specific', value: 7.99 },
+    ],
+    shipsToLithuania: false,
+  }
+]

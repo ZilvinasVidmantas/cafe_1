6e170a9104924cb843b0ceb81e836fd248958afc
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-20 20:46:50 +0300

    lesson 43: people task done

---
+++ JavaScript dom modifikavimas/43 pamoka - turinio kūrimas komponentais/js/data/peopleData.js
@@ -0,0 +1,29 @@
+const peopleData = [
+  {
+    name: 'Mantas',
+    surname: 'Kačerauskas',
+    title: 'Direktorius',
+    email: 'mantas@prodivi.lt',
+    mobile: '+370 672 89865',
+    linkedIn: 'https://www.linkedin.com/in/mantas-ka%C4%8Derauskas-295648aa/',
+    imgSrc: 'https://prodivi.lt/wp-content/uploads/2019/07/Mantas3.png'
+  },
+  {
+    name: 'Julijus',
+    surname: 'Cezaris',
+    title: 'Imperatorius',
+    email: 'julka@pizza.it',
+    mobile: '+999 999 99999',
+    linkedIn: 'https://www.linkedin.com/in/dr-julius-cesar-mamaril-aa79486/',
+    imgSrc: 'https://media-exp1.licdn.com/dms/image/C4E03AQEdz0_5J9KUQA/profile-displayphoto-shrink_800_800/0/1517705432331?e=1640217600&v=beta&t=Tr90TqaAbK0eqBBJrqIt0kFNcIFNxIhs_N9nvt4RWcM'
+  },
+  {
+    name: 'Mantas',
+    surname: 'Kačerauskas',
+    title: 'Direktorius',
+    email: 'mantas@prodivi.lt',
+    mobile: '+370 672 89865',
+    linkedIn: 'https://www.linkedin.com/in/mantas-ka%C4%8Derauskas-295648aa/',
+    imgSrc: 'https://prodivi.lt/wp-content/uploads/2019/07/Mantas3.png'
+  }
+];

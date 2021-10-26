4ae965f1ba80496548e1c5087520d3e699cfedbe
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-26 18:17:54 +0300

    lesson 46: tasks

---
+++ JavaScript dom modifikavimas/46 pamoka - CRUD 1/js/main.js
@@ -0,0 +1,13 @@
+/*
+  1. Sukurti lentelės komponentą faile <table-component.js>, su minimaliu atvaizdavmu (hard-code)
+    - komponentui perduotas savybes daugoti <props>
+    - komponento elementą saugoti savybėje - <htmlElement>
+    - komponento atvaizdavimo logiką atlikti metode <render>
+
+  2. Prijungti sukurto lentelės komponento-elementą prie egzistuojančio html kodo
+    - įdėti turinį į elementą su id="root"
+  3. Įgalinti lentelės komponento stulpelių pavadinimų atvaizdavimą, pagal duomenis: ['stulpelio pavadinimas1', 'stulpelio pavadinimas2', 'stulpelio pavadinimas3']
+    -
+  4. Naudojant userDataArr suformuoti duomenis tinkamus atvaizduoti lentelės eilutėms
+
+*/

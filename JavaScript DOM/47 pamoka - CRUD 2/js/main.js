92deab5ad270412ab2c05873aa961c3133c35e5c
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-27 20:47:56 +0300

    lesson 47: additional form tasks

--- JavaScript DOM/47 pamoka - CRUD 2/js/main.js
+++ JavaScript DOM/47 pamoka - CRUD 2/js/main.js
@@ -13,11 +13,18 @@ rootElement.appendChild(userTableComponent.htmlElement);
 
 
 /*
-  1. Sukurti formos komponentą faile <form-component.js>, su minimaliu atvaizdavmu (hard-code)
+  1. Sukurti formos komponentą faile <form-component.js>, su (hard-code) įvesties laukais ir submit mygtuku
     - komponentui perduotas savybes saugoti <props>
     - komponento elementą saugoti savybėje - <htmlElement>
     - komponento atvaizdavimo logiką atlikti metode <render>
 
-  2. Prijungti sukurto formos komponento-elementą prie egzistuojančio html kodo
+  2. Prijungti sukurtos formos komponento-elementą prie egzistuojančio html kodo
     - įdėti komponento htmlElement į elementą su id="root"
+
+  3. Sukurti metodą 'handleSubmit', ir vykdyti jį, kuomet form'ą yra submit'inama
+    - uždėti įvykio klausiklį, 'constructor' metode
+    - sustabdyti užklausos darymą, jog nebūtų perkraunama naršyklė
+
+  4. Įgalinti metodo 'handleSubmit' logiką, jog jis atspausdintų visų formos įvesties laukų reikšmes
+
 */

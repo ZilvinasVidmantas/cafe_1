2dc39bf85eafb3bc46f40ee90dc246da60631ccf
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-16 19:56:26 +0200

    lesson 56: esential files left

---
+++ React/56 pamoka - react įvadas/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <main>
+        <h1>Čia yra aplikacija</h1>
+      </main>
+    );
+  }
+}
+
+export default App;

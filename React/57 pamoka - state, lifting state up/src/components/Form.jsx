498cc4a10d3a1a13d38658bc19edf53868e602ee
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-17 20:24:29 +0200

    lesson 57: aditional field task done

--- React/57 pamoka - state, lifting state up/src/components/Form.jsx
+++ React/57 pamoka - state, lifting state up/src/components/Form.jsx
@@ -2,41 +2,78 @@ import React from 'react';
 import InputField from './InputField';
 class Form extends React.Component {
   state = {
-    password: '',
-    email: '',
-    formDataJSON: '' 
+    name: 'Serbentautas',
+    surname: 'Bordiūras',
+    age: '19',
+    password: 'Labas123',
+    email: 'serbentas@gmail.com',
+    formDataJSON: ''
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
     this.setState({
       formDataJSON: JSON.stringify({
+        name: this.state.name,
+        surname: this.state.surname,
+        age: this.state.age,
+        password: this.state.password,
         email: this.state.email,
-        password: this.state.email
-      })
+      }, undefined, 1)
     })
   }
 
-  handleEmailChange = (email) => this.setState({email });
-  
+  handleNameChange = (name) => this.setState({ name });
+
+  handleSurnameChange = (surname) => this.setState({ surname });
+
+  handleAgeChange = (age) => this.setState({ age });
+
+  handleEmailChange = (email) => this.setState({ email });
+
   handlePasswordChange = (password) => this.setState({ password });
-  
+
 
   render() {
+    console.log(this.state.formDataJSON);
     return (
       <form onSubmit={this.handleSubmit}>
         <InputField
+          name="name"
+          type="text"
+          label="Vardas"
+          id="name-field-id"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
+        <InputField
+          name="surname"
+          type="text"
+          label="Pavardė"
+          id="surname-field-id"
+          value={this.state.surname}
+          onChange={this.handleSurnameChange}
+        />
+        <InputField
+          name="age"
+          type="number"
+          label="Amžius"
+          id="age-field-id"
+          value={this.state.age}
+          onChange={this.handleAgeChange}
+        />
+        <InputField
+          name="email"
           type="email"
           label="El. Paštas"
-          name="email"
           id="email-field-id"
           value={this.state.email}
           onChange={this.handleEmailChange}
         />
         <InputField
+          name="password"
           type="password"
           label="Slaptažodis"
-          name="password"
           id="password-field-id"
           value={this.state.password}
           onChange={this.handlePasswordChange}

dd0d5de09c20dd4d6dcc7fe833a6d4e19e910262
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-17 18:03:32 +0200

    lesson 57: initial

---
+++ React/57 pamoka - state, lifting state up/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Field from './components/Field';
+
+class App extends Component {
+
+  render() {
+    return (
+      <main>
+        <h1>Čia yra aplikacija</h1>
+        <Field
+          type="password"
+          label="Slaptažodis"
+          name="password"
+          id="password-field-id"
+        />
+        <Field
+          type="email"
+          label="El. Paštas"
+          name="email"
+          id="email-field-id"
+        />
+      </main>
+    );
+  }
+}
+
+export default App;

4b37af409e4a1b5bfbb3a2979369bd73604a6fb9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-22 18:03:22 +0200

    lesson 59: init

---
+++ React/59 pamoka - validacija/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Form from './components/Form';
+
+class App extends Component {
+
+  render() {
+    return (
+      <main>
+        <h1>Čia yra aplikacija</h1>
+        <Form
+          title="Registracija"
+          submitText="Registruotis"
+          fields={[
+            { name: 'name', type: 'text', label: 'Vardas' },
+            { name: 'surname', type: 'text', label: 'Pavardė' },
+            { name: 'age', type: 'number', label: 'Amžius' },
+            { name: 'password', type: 'password', label: 'Slaptažodis' },
+            { name: 'email', type: 'email', label: 'El. paštas' },
+          ]}
+        />
+      </main>
+    );
+  }
+}
+
+export default App;
+
---
+++ React/59 pamoka - validacija/src/components/InputField.jsx
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react';
+
+class InputField extends PureComponent {
+
+  handleInputChange = (e) => this.props.onChange(e.target.value);
+
+  render() {
+    const { type, name, label, id, value } = this.props;
+
+    return (
+      <div>
+        <label htmlFor={id}>{label}</label> <br />
+        <input
+          type={type}
+          name={name}
+          id={id}
+          value={value}
+          onChange={this.handleInputChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default InputField;

de5846719b77acc35c56f1d48a587466ea4ba9e7
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-23 20:50:04 +0200

    lesson 60: for submit task

--- React/60 pamoka - validacija 2/src/App.jsx
+++ React/60 pamoka - validacija 2/src/App.jsx
@@ -2,21 +2,16 @@ import React, { Component } from 'react';
 import validator from 'validator';
 import Form from './components/Form';
 
-// 2-32 simbolių, tik raidės ir tarpai 
 const nameValidator = (val) => /^[a-zA-Z ąčęėįšųūžĄČĘĖĮŠŲŪŽ]{2,32}$/.test(val)
   ? null
   : 'Privalo būti nuo 2 iki 32 lietuviškų raidžių';
-// 2-32 simbolių, tik raidės ir tarpai 
 const surnameValidator = (val) => /^[a-zA-Z ąčęėįšųūžĄČĘĖĮŠŲŪŽ]{2,32}$/.test(val)
   ? null
   : 'Privalo būti nuo 2 iki 32 lietuviškų raidžių';
-// 1 - 150 
 const ageValidator = (val) => validator.isInt(val, { min: 1, max: 150 }) ? null : 'Amžius privalo būti nuo 1 iki 150 metų.';
-// 1 Didžioji, 1 Mažoji, min 6 simboliai
 const passwordValidator = (val) => validator.isStrongPassword(val, { minLength: 6, minSymbols: 0, minNumbers: 0 })
   ? null
   : 'Slapažodis privalo būti nors 6 simbolių, su nors viena mažąja ir nors viena didžiąja raide.';
-// Turi būt paštas
 const emailValidator = (val) => validator.isEmail(val) ? null : 'Neteisingas pašto formatas';
 
 const formFields = [
@@ -28,6 +23,20 @@ const formFields = [
 ];
 
 class App extends Component {
+  doStuff = (data) => {
+    console.log('Funkcija vykdoma App komponente')
+    console.log(data);
+    console.log('Funkcija vykdoma App komponente')
+  }
+
+  /*
+    įgalinkite komponente Form, jog funkcija <doStuff> išsikviestų, kuomet
+    submit'iname formą;
+
+    P.S.:
+      * Perduokite funkciją naudodami props'ą: onSubmit
+  
+  */
 
   render() {
     return (

793f0782da01763d4ac94602c3193aebdf29748b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-23 21:59:16 +0200

    lesson 60: submitBtn styles

--- React/60 pamoka - validacija 2/src/components/Form/index.jsx
+++ React/60 pamoka - validacija 2/src/components/Form/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import InputField from './InputField';
+import InputField from '../InputField';
+import styles from './styles.module.scss';
 
 class Form extends React.Component {
 

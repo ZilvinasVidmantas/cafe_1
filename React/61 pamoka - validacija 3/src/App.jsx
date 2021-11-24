54746d6f8520c27120da1013978c5a3d8c601924
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-24 18:32:51 +0200

    lesson 61: From initial validation & conditional submitBtn styles & touched implementation

--- React/61 pamoka - validacija 3/src/App.jsx
+++ React/61 pamoka - validacija 3/src/App.jsx
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import validator from 'validator';
 import Form from './components/Form';
 
-const nameValidator = (val) => /^[a-zA-Z ąčęėįšųūžĄČĘĖĮŠŲŪŽ]{2,32}$/.test(val)
+const nameValidator = (val) => val && /^[a-zA-Z ąčęėįšųūžĄČĘĖĮŠŲŪŽ]{2,32}$/.test(val)
   ? null
   : 'Privalo būti nuo 2 iki 32 lietuviškų raidžių';
-const surnameValidator = (val) => /^[a-zA-Z ąčęėįšųūžĄČĘĖĮŠŲŪŽ]{2,32}$/.test(val)
+const surnameValidator = (val) => val && /^[a-zA-Z ąčęėįšųūžĄČĘĖĮŠŲŪŽ]{2,32}$/.test(val)
   ? null
   : 'Privalo būti nuo 2 iki 32 lietuviškų raidžių';
-const ageValidator = (val) => validator.isInt(val, { min: 1, max: 150 }) ? null : 'Amžius privalo būti nuo 1 iki 150 metų.';
-const passwordValidator = (val) => validator.isStrongPassword(val, { minLength: 6, minSymbols: 0, minNumbers: 0 })
+const ageValidator = (val) => val && validator.isInt(val, { min: 1, max: 150 }) ? null : 'Amžius privalo būti nuo 1 iki 150 metų.';
+const passwordValidator = (val) => val && validator.isStrongPassword(val, { minLength: 6, minSymbols: 0, minNumbers: 0 })
   ? null
   : 'Slapažodis privalo būti nors 6 simbolių, su nors viena mažąja ir nors viena didžiąja raide.';
-const emailValidator = (val) => validator.isEmail(val) ? null : 'Neteisingas pašto formatas';
+const emailValidator = (val) => val && validator.isEmail(val) ? null : 'Neteisingas pašto formatas';
 
 const formFields = [
   { name: 'name', type: 'text', label: 'Vardas', validator: nameValidator },

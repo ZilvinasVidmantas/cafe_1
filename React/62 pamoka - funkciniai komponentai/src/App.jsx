a99e2caba471e7eade9ad297b0448ec567929623
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-25 19:26:49 +0200

    lesson 62: arithmetic actions with select

--- React/62 pamoka - funkciniai komponentai/src/App.jsx
+++ React/62 pamoka - funkciniai komponentai/src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react';
 
+const arithmeticActions = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+}
+
 const App = () => {
   const [counter, setCounter] = useState(5);
+  const [amount, setAmount] = useState(2);
+  const [arithmeticAction, setArtimethicAction] = useState();
 
   return (
-    <div style={{ textAlign: 'center'}}>
+    <div style={{ textAlign: 'center' }}>
       <h1>{counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>Increase</button>
-      <button onClick={() => setCounter(counter - 1)}>Decrease</button>
+      <input type="number" value={amount} onChange={(e) => setAmount(Number(e.target.value))} />
+      <select value="ccc" onChange={(event) => {/* Ką daryti, jeigu keičiasi pasirinkta reikšmė*/ }}>
+        <option value="aaa">Add</option>
+        <option value="bbb">Subtract</option>
+        <option value="ccc">Multiply</option>
+        <option value="ddd">Divide</option>
+      </select>
+      <button onClick={() => { }}>Count</button>
     </div>
   );
 }
@@ -15,6 +30,6 @@ const App = () => {
 export default App;
 
 /*
-  Sukurkite input'ą, kuriame saugokite skaičių, susietą dvipusiu(two-way) binding būdų
-  Paspaudus paddinimo/sumažinimo mygtukus, turi būti didinama/mažina pagal įvesties lauke įrašytą reikšmę 
+  Įgalinkite logiką, jog paspaudus mygtuką 'count', būtų atliekaas veiksmas, kuris nurodytas select'e
+
 */

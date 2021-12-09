4d599aaa25a12288537f0057c6626ad14a0fe018
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-12-09 18:49:59 +0200

    lesson 70: left menu part

--- React/AirBnB/src/pages/ApartmentGridPage.jsx
+++ React/AirBnB/src/pages/ApartmentGridPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button, Fab } from '@mui/material';
 import { styled, useTheme } from '@mui/material/styles';
 import { useLocation } from "react-router-dom";
 import { addDays, formatDateRange } from "../helpers/dateHelpers";
 import Carousel from 'react-material-ui-carousel';
+import HouseIcon from '@mui/icons-material/House';
+import CapsuleButton from '../components/buttons/CapsuleButton';
 
 const random = (from, to) => from + Math.floor(Math.random() * (to - from))
 
@@ -130,7 +132,53 @@ const ApartmentCard = ({ id, title, distance, images, liked, openDateRange, pric
       </Box>
     </Box>
   </Box>
-)
+);
+
+
+const ApartmentButton = styled(Button)(({ theme }) => ({
+  backgroundColor: 'none',
+  textTransform: 'none',
+  color: theme.palette.text.secondary,
+  ':hover': {
+    backgroundColor: theme.palette.action.hover,
+  },
+}))
+
+const ApartmentGridHeader = () => {
+
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'space-between' }} >
+
+
+      <Box sx={{ display: 'flex', mt: 4, mb: 2 }}>
+        <Box sx={{ pb: 1, borderBottom: 1 }}>
+          <ApartmentButton>
+            <HouseIcon sx={{ verticalAlign: 'bottom', mr: 0.5 }} />
+            <Typography sx={{ verticalAlign: 'bottom' }} color="text.primary">Nuosavi namai</Typography>
+          </ApartmentButton>
+        </Box>
+        <Box sx={{ pb: 1 }}>
+          <ApartmentButton>
+            <Typography sx={{ verticalAlign: 'bottom' }} color="text.secondary">Butai</Typography>
+          </ApartmentButton>
+
+        </Box>
+        <Box sx={{ pb: 1 }}>
+          <ApartmentButton>
+            <Typography sx={{ verticalAlign: 'bottom' }} color="text.secondary">Kotedžai</Typography>
+          </ApartmentButton>
+        </Box>
+      </Box>
+      <Box sx={{ display: 'flex' }}>
+        <Fab variant="extended">Bet kada</Fab>
+        <Fab variant="extended">Svečiai</Fab>
+        <Fab variant="extended">Filtrai</Fab>
+      </Box>
+    </Box >
+  );
+};
+
+
 
 const ApartmentGridPage = () => {
   const location = useLocation();
@@ -138,25 +186,14 @@ const ApartmentGridPage = () => {
   console.log(location);
 
   return (
-    <ApartmentGrid sx={{ my: 4 }}>
-      {apartments.map(apartment => <ApartmentCard key={apartment.id} {...apartment} />)}
-    </ApartmentGrid>
+    <Box>
+      <ApartmentGridHeader />
+      <ApartmentGrid sx={{ my: 4 }}>
+        {apartments.map(apartment => <ApartmentCard key={apartment.id} {...apartment} />)}
+      </ApartmentGrid>
+    </Box>
   );
 };
 
 export default ApartmentGridPage;
 
-/*
-  Sukurkite Kortelių Grid'ą
-  pertrauka 10min
-  Kortelė:
-    // * Basic grid - 20:10
-    // * Viena uotrauka 20:25
-    // * pertauka 21:00
-    * Nuotraukų slider'is 21:10
-      * https://www.npmjs.com/package/react-material-ui-carousel
-      *
-    * Datos formatas rytoj
-    * like'o formatas rytoj
-    * Likęs stilizavimas rytoj
-*/

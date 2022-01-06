17c4f749edb55d13073fd8d6d546bfbde1805544
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-06 19:25:49 +0200

    feat(82): login form fields binded with component state

--- React/82 pamoka - redux combine reducers/src/pages/login-page.jsx
+++ React/82 pamoka - redux combine reducers/src/pages/login-page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TextField,
   Grid,
@@ -10,41 +10,57 @@ import AuthForm from '../components/auth-form';
 
   Surišus šiuos duomenis, atvaspausdinti juos konsolėje,
   kiekvieną kart pakeitus nors vieną ir įvesties laukų
+
+  15min
+  10min
+  tęsiame 19:20
 */
 
-const LoginPage = () => (
-  <AuthForm
-    title="Prisijungti"
-    linkTo="/register"
-    linkTitle="Neturite paskyros? Registruokitės"
-  >
-    <Grid container spacing={4}>
-      <Grid item xs={12}>
-        <TextField
-          variant="outlined"
-          required
-          fullWidth
-          id="email"
-          label="El. paštas"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  console.log({
+    email, password,
+  });
+
+  return (
+    <AuthForm
+      title="Prisijungti"
+      linkTo="/register"
+      linkTitle="Neturite paskyros? Registruokitės"
+    >
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="email"
+            label="El. paštas"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12} sx={{ mb: 4 }}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            name="password"
+            label="Slaptažodis"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={12} sx={{ mb: 4 }}>
-        <TextField
-          variant="outlined"
-          required
-          fullWidth
-          name="password"
-          label="Slaptažodis"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-      </Grid>
-    </Grid>
-  </AuthForm>
-);
+    </AuthForm>
+  );
+};
 
 export default LoginPage;

1fbad767cdf6ebba8e86c985ab2a1658aa60dc33
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-10 18:27:48 +0200

    feat(83): created auth selector and used in Navbar

--- React/83 pamoka - prisijungimas/src/components/partials/navbar/index.jsx
+++ React/83 pamoka - prisijungimas/src/components/partials/navbar/index.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import { AppBar, Container, Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { authSelector } from '../../../store/auth/selectors';
 import Link from './navbar-link';
 
-const Navbar = () => (
-  <AppBar position="sticky" sx={{ height: 56 }}>
-    <Container sx={{
-      height: '100%',
-      display: 'flex',
-      justifyContent: 'space-between',
-    }}
-    >
-      <Box sx={{ display: 'flex' }}>
-        <Link to="/">Home</Link>
-        <Link to="/users-panel">Users panel</Link>
-      </Box>
-      <Box sx={{ display: 'flex' }}>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </Box>
-    </Container>
-  </AppBar>
-);
+const Navbar = () => {
+  const auth = useSelector(authSelector);
+  console.log(auth);
+
+
+  return (
+    <AppBar position="sticky" sx={{ height: 56 }}>
+      <Container sx={{
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
+      >
+        <Box sx={{ display: 'flex' }}>
+          <Link to="/">Home</Link>
+          <Link to="/users-panel">Users panel</Link>
+        </Box>
+        <Box sx={{ display: 'flex' }}>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </Box>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default Navbar;

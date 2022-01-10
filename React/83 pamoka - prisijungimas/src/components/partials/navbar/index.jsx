c0122d466e2fc40efd76401d1ac13d562a157234
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-10 18:50:11 +0200

    task-done(83): authenticated links in navbar

--- React/83 pamoka - prisijungimas/src/components/partials/navbar/index.jsx
+++ React/83 pamoka - prisijungimas/src/components/partials/navbar/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { AppBar, Container, Box } from '@mui/material';
+import {
+  AppBar, Container, Box, Button,
+} from '@mui/material';
 import { useSelector } from 'react-redux';
 import { authSelector } from '../../../store/auth/selectors';
 import Link from './navbar-link';
 
 const Navbar = () => {
   const auth = useSelector(authSelector);
-  console.log(auth);
-  /*
-    1. Kuomet vartotojas prisijungęs, paslėpti Login ir Register nuorodas
-    2. Vietoj Login ir Resgiter nuorodų įdėti mygtuką Su užrašu "Logout"
-  */
 
   return (
     <AppBar position="sticky" sx={{ height: 56 }}>
@@ -24,10 +21,16 @@ const Navbar = () => {
           <Link to="/">Home</Link>
           <Link to="/users-panel">Users panel</Link>
         </Box>
-        <Box sx={{ display: 'flex' }}>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </Box>
+        {
+          auth.loggedIn
+            ? <Button color="secondary" variant="contained" sx={{ my: 1 }}>Logout</Button>
+            : (
+              <Box sx={{ display: 'flex' }}>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+              </Box>
+            )
+        }
       </Container>
     </AppBar>
   );

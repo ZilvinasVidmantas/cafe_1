8af94c0a1d487bc65d052ad631120714bab6b737
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-10 20:11:26 +0200

    feat(83): redux auth store synchronized with sessionStorage

--- React/83 pamoka - prisijungimas/src/store/auth/index.js
+++ React/83 pamoka - prisijungimas/src/store/auth/index.js
@@ -1,32 +1,40 @@
 /* eslint-disable no-param-reassign */
 import changeState from 'immer';
 import { LOGIN_SUCCESS, LOGOUT } from './action-types';
+import SessionStorage from '../../libs/SessionStorage';
 
-const initState = {
+const initState = SessionStorage.get('auth') ?? {
   loggedIn: false,
   token: null,
   user: null,
 };
+console.log(initState);
 
 // eslint-disable-next-line default-param-last
-const authReducer = (state = initState, action) => {
+const authReducer = (oldState = initState, action) => {
   switch (action.type) {
     case LOGIN_SUCCESS: {
-      return changeState(state, (newState) => {
-        newState.loggedIn = true;
-        newState.token = action.payload.token;
-        newState.user = action.payload.user;
+      const newState = changeState(oldState, (state) => {
+        state.loggedIn = true;
+        state.token = action.payload.token;
+        state.user = action.payload.user;
       });
+      SessionStorage.set('auth', newState);
+
+      return newState;
     }
     case LOGOUT: {
-      return changeState(state, (newState) => {
-        newState.loggedIn = false;
-        newState.token = null;
-        newState.user = null;
+      const newState = changeState(oldState, (state) => {
+        state.loggedIn = false;
+        state.token = null;
+        state.user = null;
       });
+      SessionStorage.clear('auth');
+
+      return newState;
     }
     default:
-      return state;
+      return oldState;
   }
 };
 

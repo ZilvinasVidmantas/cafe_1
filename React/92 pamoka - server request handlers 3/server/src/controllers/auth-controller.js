e2658d52f6908731a266f370a99b6ebc99d1d3ce
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-26 18:22:58 +0200

    feat(92): initial - json web token

---
+++ React/92 pamoka - server request handlers 3/server/src/controllers/auth-controller.js
@@ -0,0 +1,83 @@
+import database from '../database/index.js';
+import { v4 as createId } from 'uuid';
+
+export const login = (req, res) => {
+  const { email, password } = req.body;
+  const { users } = database.data;
+
+  const foundUser = users.find(x => x.email === email);
+  if (!foundUser) {
+    // Nerastas Vartotojas
+    res.status(404).json({
+      message: 'Vartotojas su tokiu paštu nerastas'
+    });
+    return;
+  }
+
+  if (foundUser.password === password) {
+    delete foundUser.password;
+    // Viskas gerai
+    res.status(200).json({
+      user: foundUser,
+      token: 'Kazkada busiu tikras tokenas'
+    });
+    return;
+  }
+
+  // Neteisingas slaptažodis
+  res.status(400).json({
+    message: 'Neteisingas slaptažodis'
+  });
+};
+
+export const register = (req, res) => {
+  const { email, name, surname, password, repeatPassword } = req.body;
+  if (password !== repeatPassword) {
+    res.status(400).json({
+      message: 'Slaptažodžiai nesutampa'
+    });
+    return;
+  }
+
+  const userExists = database.data.users.find(x => x.email === email);
+
+  if (userExists) {
+    res.status(400).json({
+      message: 'Vartotojas su tokiu paštu jau egzistuoja'
+    });
+    return;
+  }
+
+  const user = {
+    id: createId(),
+    email,
+    name,
+    surname,
+    password,
+    role: 'USER'
+  };
+
+  database.data.users.push(user);
+  database.write();
+
+  res.status(200).json({
+    user,
+    token: 'sdfhgiosdfhgosfdob9viunfgboifg65+641+65'
+  });
+}
+
+export const checkEmail = (req, res) => {
+  const { email } = req.query;
+  const emailIsTaken = Boolean(database.data.users.find(x => x.email === email));
+  if (emailIsTaken) {
+    res.status(400).json({
+      available: false,
+      message: 'Vartotojas su tokiu paštu jau egzistuoja'
+    });
+  } else {
+    res.status(200).json({
+      available: true,
+      message: 'Vartotojas galimas'
+    });
+  }
+}
---
+++ React/92 pamoka - server request handlers 3/server/src/routes/auth-router.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { 
+  login,
+  register,
+  checkEmail,
+} from '../controllers/auth-controller.js';
+
+const router = express.Router();
+
+router.post('/register', register);
+
+router.post('/login', login);
+
+router.get('/check-email', checkEmail);
+
+export default router;

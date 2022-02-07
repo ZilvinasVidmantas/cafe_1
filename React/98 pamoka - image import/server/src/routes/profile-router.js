01be1ba24f361d696e0803550333ad7c0f6082a7
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-07 19:39:38 +0200

    feat(98): upload img middleware exported to separate file

--- React/98 pamoka - image import/server/src/routes/profile-router.js
+++ React/98 pamoka - image import/server/src/routes/profile-router.js
@@ -1,24 +1,10 @@
 import { Router } from 'express';
 import { updateImage } from '../controllers/profile-controller.js';
 import authMiddleware from '../middlewares/auth-middleware.js';
-import multer from 'multer';
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/images')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now();
-    const { originalname} = file;
-    const ext = originalname.slice(originalname.indexOf('.'));
-    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
-  }
-})
-
-const uploadMiddleware = multer({ storage });
+import imgUploadMiddleware from '../middlewares/img-upload-middleware.js';
 
 const router = Router();
 
-router.put('/img', authMiddleware, uploadMiddleware.single('img'), updateImage);
+router.put('/img', authMiddleware, imgUploadMiddleware.single('img'), updateImage);
 
 export default router;

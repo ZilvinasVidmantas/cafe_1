7bbefc47d09fed5a510cf4acd71f5bc54c8ddf0a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-07 20:44:34 +0200

    feat(98): exported delete file function

--- React/98 pamoka - image import/server/src/controllers/profile-controller.js
+++ React/98 pamoka - image import/server/src/controllers/profile-controller.js
@@ -1,23 +1,16 @@
 import database from '../database/index.js';
-import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-
-const filePath = fileURLToPath(import.meta.url);
-const rootDirPath = filePath.slice(0, filePath.indexOf('src'));
+import { removeFile } from '../helpers/file-helpers.js';
 
 export const updateImage = (req, res) => {
   const user = database.data.users.find(x => x.email === req.user.email);
-  const { SERVER_DOMAIN, SERVER_PORT, IMG_PATH, PUBLIC_PATH } = process.env;
+  const { SERVER_DOMAIN, SERVER_PORT, IMG_PATH } = process.env;
   const imgSrc = `${SERVER_DOMAIN}:${SERVER_PORT}${IMG_PATH}${req.file.filename}`;
   if (user.imgSrc) {
-    const imgPathRelative = user.imgSrc.slice(user.imgSrc.indexOf(IMG_PATH));
-    const fullPath = path.join(rootDirPath, PUBLIC_PATH, imgPathRelative);
-    if (fs.existsSync(fullPath)) {
-      fs.unlinkSync(fullPath)
-    }
+    const relativePath = user.imgSrc.slice(user.imgSrc.indexOf(IMG_PATH));
+    removeFile(relativePath);
   }
   user.imgSrc = imgSrc;
   database.write();
+  
   res.status(200).json({ message: 'Image uploaded', imgSrc });
 }

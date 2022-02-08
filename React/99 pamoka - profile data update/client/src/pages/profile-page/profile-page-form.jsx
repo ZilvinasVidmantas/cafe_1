53b96e0fc8903bc561a1f69239b677b0cf84a093
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-08 18:21:08 +0200

    feat(99): initial formik setup

--- React/99 pamoka - profile data update/client/src/pages/profile-page/profile-page-form.jsx
+++ React/99 pamoka - profile data update/client/src/pages/profile-page/profile-page-form.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useFormik } from 'formik';
+// import * as yup from 'yup';
+
 import {
   Box,
   TextField,
@@ -15,18 +18,57 @@ const Form = styled('form')(({ theme }) => ({
   },
 }));
 
-const ProfilePageForm = ({ name, surname, email }) => (
-  <Form>
-    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
-      <TextField label="Vardas" size="small" value={name} disabled />
-      <TextField label="Pavardė" size="small" value={surname} disabled />
-      <TextField label="Paštas" size="small" value={email} disabled />
-      <TextField label="Slaptažodis" size="small" disabled />
-    </Box>
-    <Box sx={{ display: 'flex', justifyContent: 'center', pt: 2 }}>
-      <Button variant="contained" color="primary">Redaguoti</Button>
-    </Box>
-  </Form>
-);
+const ProfilePageForm = ({ name, surname, email }) => {
+  const {
+    values,
+    touched,
+    errors,
+    dirty,
+    handleChange,
+  } = useFormik({
+    initialValues: {
+      name,
+      surname,
+      email,
+    },
+  });
+  return (
+    <Form>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+        <TextField
+          label="Vardas"
+          size="small"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
+        <TextField
+          label="Pavardė"
+          size="small"
+          name="surname"
+          value={values.surname}
+          onChange={handleChange}
+        />
+        <TextField
+          label="Paštas"
+          size="small"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', pt: 2 }}>
+        <Button variant="contained" color="primary" disabled={!dirty}>Redaguoti</Button>
+      </Box>
+      <pre style={{ position: 'fixed', top: 150, right: 150 }}>
+        { JSON.stringify({
+          values,
+          touched,
+          errors,
+        }, null, 2)}
+      </pre>
+    </Form>
+  );
+};
 
 export default ProfilePageForm;

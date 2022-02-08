f4dc6c7463666154254e90ba0068530c38f2b3c9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-08 20:57:01 +0200

    feat(99): handle profile update in server

--- React/99 pamoka - profile data update/server/src/controllers/profile-controller.js
+++ React/99 pamoka - profile data update/server/src/controllers/profile-controller.js
@@ -4,7 +4,7 @@ import { removeFile } from '../helpers/file-helpers.js';
 
 export const updateImage = (req, res) => {
   const user = database.data.users.find(x => x.email === req.user.email);
-  const { SERVER_DOMAIN, SERVER_PORT, IMG_PATH} = process.env;
+  const { SERVER_DOMAIN, SERVER_PORT, IMG_PATH } = process.env;
   const imgSrc = `/${IMG_PATH}/${req.file.filename}`;
   if (user.imgSrc) {
     removeFile(user.imgSrc);
@@ -15,5 +15,22 @@ export const updateImage = (req, res) => {
   res.status(200).json({
     message: 'Image uploaded',
     user: new UserViewModel(user),
+  });
+}
+
+export const updateProfile = (req, res) => {
+  const { name, surname, email } = req.body;
+
+  const foundUser = database.data.users.find(x => x.email === req.user.email);
+
+  if (name) foundUser.name = name;
+  if (surname) foundUser.surname = surname;
+  if (email) foundUser.email = email;
+
+  database.write();
+
+  res.status(200).json({
+    message: 'Profile updated',
+    user: new UserViewModel(foundUser),
   });
 }

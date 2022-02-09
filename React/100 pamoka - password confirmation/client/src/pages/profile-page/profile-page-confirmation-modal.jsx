fb390f0a2adaf10d6688972cda90c8dfdeded7a9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-09 18:34:41 +0200

    feat(100): env files

---
+++ React/100 pamoka - password confirmation/client/src/pages/profile-page/profile-page-confirmation-modal.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  Paper,
+  Modal,
+  Typography,
+  styled,
+} from '@mui/material';
+
+const Form = styled(Paper)(({ theme }) => ({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  p: theme.spacing(4),
+}));
+
+const ProfilePageConfimationModal = ({ open, handleClose }) => (
+  <Modal
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description"
+  >
+    <Form>
+      <Typography id="modal-modal-title" variant="h6" component="h2">
+        Text in a modal
+      </Typography>
+      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+      </Typography>
+    </Form>
+  </Modal>
+);
+
+export default ProfilePageConfimationModal;

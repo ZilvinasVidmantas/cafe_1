d39b72d560aaa1a6dd7f0c084ff7a531230b5ad7
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-10 18:47:31 +0200

    feat(101): search bar

--- React/101 pamoka - product page view/client/src/components/layouts/page-layout/page-layout-navbar.jsx
+++ React/101 pamoka - product page view/client/src/components/layouts/page-layout/page-layout-navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {
-  AppBar, Container, Box,
+  AppBar,
+  Container,
+  Box, Badge,
+  Divider,
 } from '@mui/material';
 import { useSelector } from 'react-redux';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Search from './page-layout-search';
 import { selectAuth } from '../../../store/auth';
 import NavbarLink from '../../navbar-link';
 import routes from '../../../routing/routes';
@@ -27,7 +32,10 @@ const PageLayoutNavbar = () => {
         <Box sx={{ display: 'flex' }}>
           <NavbarLink to={routes.HomePage}>Home</NavbarLink>
         </Box>
-        {
+        <Search />
+
+        <Box sx={{ display: 'flex', alignItems: 'stretch' }}>
+          {
           loggedIn
             ? <AuthMenu />
             : (
@@ -37,6 +45,19 @@ const PageLayoutNavbar = () => {
               </Box>
             )
           }
+          <Divider
+            orientation="vertical"
+            light
+            flexItem
+            sx={{
+              borderColor: 'common.white', my: 2, mr: 2,
+            }}
+            variant="middle"
+          />
+          <Badge badgeContent={4} color="secondary" sx={{ alignSelf: 'center' }}>
+            <ShoppingCartIcon color="inherit" />
+          </Badge>
+        </Box>
       </Container>
     </AppBar>
   );

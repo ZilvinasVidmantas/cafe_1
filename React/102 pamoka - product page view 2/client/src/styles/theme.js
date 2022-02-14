18ed382b4a6028c07508295bee7b953613880181
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-14 19:09:23 +0200

    feat(102): product grid header and styles

--- React/102 pamoka - product page view 2/client/src/styles/theme.js
+++ React/102 pamoka - product page view 2/client/src/styles/theme.js
@@ -1,15 +1,27 @@
 import { createTheme } from '@mui/material';
 
 const theme = createTheme({
+  shape: {
+    borderRadius: 0,
+  },
   palette: {
     background: {
       default: '#fafafa',
+    },
+    primary: {
+      main: '#252525',
+      dark: '#151515',
+      light: '#353535',
+      contrastText: '#fafafa',
     },
   },
   mixins: {
     drawer: {
       width: 240,
     },
+  },
+  typography: {
+    fontFamily: '\'Lato\', sans-serif',
   },
 });
 

58745161ce67f31309b304f182e8e309bbbd8c8a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-15 21:42:48 +0200

    feat(103): server filter logic

--- React/103 pamoka - product context/server/src/controllers/filters-controller.js
+++ React/103 pamoka - product context/server/src/controllers/filters-controller.js
@@ -2,10 +2,27 @@ import database from '../database/index.js';
 
 export const getFilters = (req, res) => {
   let filters = database.data.filters;
-  if (req.query.id) {
-    const requiredFilterIds = [].concat(req.query.id);
-    filters = filters.filter(x => requiredFilterIds.includes(x.id));
+  const category = database.data.categories.find(x => x.id === req.query.category);
+  if (category && category.filters) {
+    filters = filters.filter(x => category.filters.includes(x.id));
   }
+
+  filters = filters.map((filter) => {
+    switch (filter.type) {
+      case 'autocomplete':
+      case 'options':
+        filter.options = database.data[filter.collection];
+        break;
+      case 'range':
+        let products = database.data.products;
+        if (category && category.id) products = products.filter(x => x.category === category.id);
+        products.sort((a, b) => a[filter.property] - b[filter.property]);
+        filter.min = products[0][filter.property] ?? 0;
+        filter.max = products[products.length - 1][filter.property] ?? 0;
+        break;
+    }
+    return filter;
+  });
   res.status(200).json(filters);
 }
 

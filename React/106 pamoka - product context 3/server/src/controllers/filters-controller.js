0ba754fd22fdb716799b125851c8da25fdca9515
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-22 21:10:18 +0200

    fix(106): server filter controller dont return filter of category which it does'nt have

--- React/106 pamoka - product context 3/server/src/controllers/filters-controller.js
+++ React/106 pamoka - product context 3/server/src/controllers/filters-controller.js
@@ -3,30 +3,39 @@ import database from '../database/index.js';
 export const getFilters = (req, res) => {
   let filters = database.data.filters;
   const category = database.data.categories.find(x => x.id === req.query.category);
-  if (category && category.filters) {
-    filters = filters.filter(x => category.filters.includes(x.id));
+
+  if (!category) {
+    res.status(200).json(filters);
+    return;
   }
 
-  filters = filters.map((filter) => {
-    switch (filter.type) {
-      case 'autocomplete':
-      case 'options':
-        filter.options = database.data[filter.collection];
-        break;
-      case 'range':
-        let products = database.data.products;
-        if (category && category.id) products = products.filter(x => x.category === category.id);
-        filter.min = 0;
-        filter.max = 0;
-        if (products.length > 0) {
-          products.sort((a, b) => a[filter.property] - b[filter.property]);
-          filter.min = Math.floor(products[0][filter.property]);
-          filter.max = Math.ceil(products[products.length - 1][filter.property]);
+  if (category.filters) {
+    filters = filters
+      .filter(x => category.filters.includes(x.id))
+      .map((filter) => {
+        switch (filter.type) {
+          case 'autocomplete':
+          case 'options':
+            filter.options = database.data[filter.collection];
+            break;
+          case 'range':
+            let products = database.data.products;
+            if (category && category.id) products = products.filter(x => x.category === category.id);
+            filter.min = 0;
+            filter.max = 0;
+            if (products.length > 0) {
+              products.sort((a, b) => a[filter.property] - b[filter.property]);
+              filter.min = Math.floor(products[0][filter.property]);
+              filter.max = Math.ceil(products[products.length - 1][filter.property]);
+            }
+            break;
         }
-        break;
-    }
-    return filter;
-  });
-  res.status(200).json(filters);
+        return filter;
+      });
+
+    res.status(200).json(filters);
+  } else {
+    res.status(200).json([]);
+  }
 }
 

ba1dfe759595a6c316b7c41a6cc393b016b1a6bc
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-23 19:38:10 +0200

    feat(107): rangeFilter Implemented

--- React/107 pamoka - product context 4/server/src/controllers/products-controller.js
+++ React/107 pamoka - product context 4/server/src/controllers/products-controller.js
@@ -1,12 +1,37 @@
 import database from '../database/index.js';
 
+const filterByRange = (products, filter, queryParams) => {
+  const min = Number(queryParams[`${filter.name}_min`]);
+  const max = Number(queryParams[`${filter.name}_max`]);
+
+  if (min) {
+    products = products.filter(x => x[filter.property] >= min);
+  }
+  if (max) {
+    products = products.filter(x => x[filter.property] <= max);
+  }
+
+  return products;
+}
+
 export const getProducts = (req, res) => {
+  console.log('---------------------------------');
+  const { products, categories, filters } = database.data;
   const {
     category: categoryId,
+    ...queryParams
   } = req.query;
 
-  const products = database.data.products.filter(x => x.category === categoryId);
+  let selectedProducts = products.filter(x => x.category === categoryId);
+  const category = categories.find(x => x.id === categoryId);
+  const categoryFilters = category.filters.map(filterId => filters.find(x => x.id === filterId));
 
-  // const category = database.categories.find(x => x.id === categoryId);
-  res.status(200).json(products);
+  categoryFilters.forEach(filter => {
+    if (filter.type === 'range') {
+      selectedProducts = filterByRange(selectedProducts, filter, queryParams);
+    }
+  });
+
+  res.status(200).json(selectedProducts);
 }
+

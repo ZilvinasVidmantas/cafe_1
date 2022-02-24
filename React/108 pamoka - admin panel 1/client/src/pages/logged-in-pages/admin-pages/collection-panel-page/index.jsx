86e3024342a1e495566751112a06a6711cb4c202
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-24 18:34:43 +0200

    feat(108): initial colection panel page view

--- React/108 pamoka - admin panel 1/client/src/pages/logged-in-pages/admin-pages/collection-panel-page/index.jsx
+++ React/108 pamoka - admin panel 1/client/src/pages/logged-in-pages/admin-pages/collection-panel-page/index.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import {
+  Typography,
+  Box,
+  Button,
+  Divider,
+} from '@mui/material';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 
-const CollectionPanelPage = () => (
-  <div>CollectionPanelPage</div>
-);
+const CollectionPanelPage = () => {
+  const {
+    state: {
+      id,
+      title,
+      data,
+    },
+    ...rest
+  } = useLocation();
+
+  const navigate = useNavigate();
+  console.log(id);
+  console.log(rest);
+
+  return (
+    <Box>
+      <Button onClick={() => navigate(-1)}>
+        <ArrowCircleLeftIcon />
+        <Typography sx={{ ml: 2 }}>Back</Typography>
+      </Button>
+      <Divider sx={{ mt: 2, mb: 1 }} />
+      <Typography
+        element="h1"
+        variant="h2"
+        sx={{ textTransform: 'capitalize', mb: 3 }}
+      >
+        {title}
+
+      </Typography>
+      <pre>
+        {JSON.stringify(data, null, 4)}
+      </pre>
+    </Box>
+  );
+};
 
 export default CollectionPanelPage;

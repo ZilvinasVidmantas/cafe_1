1ad29d4f5d43e49c3b9b82f3519c1ce8aec052c2
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-28 20:49:15 +0200

    fix(109): getUsers controller

--- React/109 pamoka - admin panel 2/server/src/controllers/users-controller.js
+++ React/109 pamoka - admin panel 2/server/src/controllers/users-controller.js
@@ -3,6 +3,6 @@ import database from '../database/index.js';
 export const getUsers = (req, res) => {
   const DB = JSON.parse(JSON.stringify(database.data));
 
-  const users = DB;
+  const users = DB.users;
   res.status(200).json(users);
 }

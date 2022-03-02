060508c659662042c17574834912f14a64a0a60d
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-02 19:58:24 +0200

    feat(111): add empty properties

--- React/111 pamoka - admin panel 4/client/src/pages/logged-in-pages/admin-pages/category-panel-page/category-panel-page-property-form/index.jsx
+++ React/111 pamoka - admin panel 4/client/src/pages/logged-in-pages/admin-pages/category-panel-page/category-panel-page-property-form/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useFormik } from 'formik';
 import {
   Typography,
   Box,
@@ -9,36 +10,77 @@ import {
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import CategoryPanelPagePropertyFormConfiguration from './category-panel-page-property-form-configuration';
 
-const CategoryPanelPagePropertyForm = () => (
-  <Paper sx={{ p: 3, mt: 2 }}>
-    <Typography sx={{ fontSize: 22, mb: 3 }}>
-      Kategorijos savybių konfigūracija
-    </Typography>
-    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
-      <CategoryPanelPagePropertyFormConfiguration />
-      <CategoryPanelPagePropertyFormConfiguration />
-      <CategoryPanelPagePropertyFormConfiguration />
-      <CategoryPanelPagePropertyFormConfiguration />
-      <CategoryPanelPagePropertyFormConfiguration />
-    </Box>
+let idCounter = 0;
 
-    <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-      <IconButton size="large" color="secondary">
-        <AddCircleIcon sx={{ fontSize: 50 }} />
-      </IconButton>
-    </Box>
+const createEmptyProperty = () => {
+  idCounter += 1;
 
-    <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        size="large"
-      >
-        Atnaujinti savybes
-      </Button>
-    </Box>
-  </Paper>
-);
+  return {
+    id: String(idCounter),
+    propertyName: '',
+    propertyType: '',
+    collection: null,
+    collectionName: null,
+  };
+};
+
+const initialValues = {
+  propertiesData: [createEmptyProperty()],
+};
+
+const CategoryPanelPagePropertyForm = () => {
+  const {
+    values: { propertiesData },
+    dirty,
+    setFieldValue,
+  } = useFormik({
+    initialValues,
+  });
+
+  const addProperty = () => {
+    console.log(propertiesData);
+    setFieldValue('propertiesData', [...propertiesData, createEmptyProperty()]);
+  };
+
+  return (
+    <Paper sx={{ p: 3, mt: 2 }}>
+      <Typography sx={{ fontSize: 22, mb: 3 }}>
+        Kategorijos savybių konfigūracija
+      </Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+        {
+          propertiesData.map((propertyData) => (
+            <CategoryPanelPagePropertyFormConfiguration
+              key={propertyData.id}
+              {...propertyData}
+            />
+          ))
+        }
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+        <IconButton
+          size="large"
+          color="secondary"
+          onClick={addProperty}
+        >
+          <AddCircleIcon sx={{ fontSize: 50 }} />
+        </IconButton>
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          size="large"
+          disabled={!dirty}
+        >
+          Atnaujinti savybes
+        </Button>
+      </Box>
+    </Paper>
+  );
+};
 
 export default CategoryPanelPagePropertyForm;

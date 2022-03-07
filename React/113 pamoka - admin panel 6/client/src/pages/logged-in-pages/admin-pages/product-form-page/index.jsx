4f62a764948f8f9cea1c5b5cb422729cefc3b87e
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-07 20:53:11 +0200

    feat(113): product form layout

--- React/113 pamoka - admin panel 6/client/src/pages/logged-in-pages/admin-pages/product-form-page/index.jsx
+++ React/113 pamoka - admin panel 6/client/src/pages/logged-in-pages/admin-pages/product-form-page/index.jsx
@@ -7,6 +7,7 @@ import {
   Divider,
   TextField,
   MenuItem,
+  Grid,
 } from '@mui/material';
 import { useFormik } from 'formik';
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
@@ -82,60 +83,64 @@ const ProductFormPage = () => {
       </Button>
       <Divider sx={{ mt: 2, mb: 1 }} />
       <Typography variant="h2" sx={{ mb: 3 }}>Kurti naują produktą</Typography>
-      <Box sx={{ display: 'flex', alignItems: 'flex-start', gap: 3 }}>
-        <Paper sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 2,
-          p: 3,
-          flexGrow: 9,
-        }}
-        >
-          <TextField
-            label="Pasirinkite kategoriją"
-            select
-            fullWidth
-            name="category"
-            value={values.category}
-            onChange={handleCategoryChange}
+      <Grid container columnSpacing={4}>
+        <Grid item xs={7}>
+
+          <Paper sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            p: 3,
+          }}
           >
-            {categories.map((x) => (
-              <MenuItem key={x.id} value={x.id}>{x.title}</MenuItem>
-            ))}
-          </TextField>
-          <Divider textAlign="left">Produkto savybės</Divider>
-          {values.properties.length > 0 && values.properties.map((x, i) => (
             <TextField
-              key={x.name}
-              label={x.name}
-              type={x.type === 'range' ? 'number' : 'text'}
-              select={x.type !== 'range'}
-              name={`properties[${i}]`}
-              onChange={(e) => handlePropertyChange(x.name, e.target.value)}
-              value={x.value}
+              label="Pasirinkite kategoriją"
+              select
+              fullWidth
+              name="category"
+              value={values.category}
+              onChange={handleCategoryChange}
             >
-              {x.type === 'range' ? null : x.data.map((d) => (
-                <MenuItem key={d.id} value={d.id}>{d.title}</MenuItem>
+              {categories.map((x) => (
+                <MenuItem key={x.id} value={x.id}>{x.title}</MenuItem>
               ))}
             </TextField>
-          ))}
-          <Button
-            type="submit"
-            variant="contained"
-            color="secondary"
-            sx={{ alignSelf: 'center' }}
-            size="large"
-          >
-            Kurti
-          </Button>
-        </Paper>
+            <Divider textAlign="left">Produkto savybės</Divider>
+            {values.properties.length > 0 && values.properties.map((x, i) => (
+              <TextField
+                key={x.name}
+                label={x.name}
+                type={x.type === 'range' ? 'number' : 'text'}
+                select={x.type !== 'range'}
+                name={`properties[${i}]`}
+                onChange={(e) => handlePropertyChange(x.name, e.target.value)}
+                value={x.value}
+              >
+                {x.type === 'range' ? null : x.data.map((d) => (
+                  <MenuItem key={d.id} value={d.id}>{d.title}</MenuItem>
+                ))}
+              </TextField>
+            ))}
+            <Button
+              type="submit"
+              variant="contained"
+              color="secondary"
+              sx={{ alignSelf: 'center' }}
+              size="large"
+            >
+              Kurti
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={5}>
 
-        <FileUploadField
-          imgDataArr={values.images}
-          onChange={handleImageChange}
-          sx={{ flexGrow: 5 }}
-        />
-      </Box>
+          <FileUploadField
+            imgDataArr={values.images}
+            onChange={handleImageChange}
+            sx={{ width: '100%' }}
+          />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
